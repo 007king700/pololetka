e1f7e39e459a2f4cb9f33dac4e761c5ba3732d9e
007king700 <62300955+007king700@users.noreply.github.com>
2024-01-19 13:01:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,111 +1,73 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = 700;
-canvas.height = 900;
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
-// global settings
-ctx.lineWidth = 10;
+//canvas settings
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'gray'
+ctx.lineWidth = 2
 
-// canvas shadows
-//ctx.shadowOffsetX = 2;
-//ctx.shadowOffsetY = 2;
-//ctx.shadowColor = 'black';
-
-// gradients
-const gradient1 = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-gradient1.addColorStop('0.2', 'pink');
-gradient1.addColorStop('0.3', 'red');
-gradient1.addColorStop('0.4', 'orange');
-gradient1.addColorStop('0.5', 'yellow');
-gradient1.addColorStop('0.6', 'green');
-gradient1.addColorStop('0.7', 'turquoise');
-gradient1.addColorStop('0.8', 'violet');
-const gradient2 = ctx.createRadialGradient(canvas.width * 0.5, canvas.height * 0.5, 70, canvas.width * 0.5, canvas.height * 0.5, 400,);
-gradient2.addColorStop('0.2', 'green');
-gradient2.addColorStop('0.5', 'red');
-gradient2.addColorStop('0.8', 'blue');
-
-// canvas pattern
-const paternImage = document.getElementById('patternImage');
-const pattern1 = ctx.createPattern(patternImage, 'no-repeat');
-
-ctx.strokeStyle = 'red';
-
-class Line {
-    constructor(canvas){
-        this.canvas = canvas;
-        this.x = Math.random() * this.canvas.width;
-        this.y = Math.random() * this.canvas.height;
-        this.history = [{x: this.x, y: this.y}];
-        this.lineWidth = Math.floor(Math.random() * 25 + 1);
-        this.hue = Math.floor(Math.random() * 360);
-        this.maxLength = Math.floor(Math.random() * 150 + 10);
-        this.speedX = Math.random() * 1 - 0.5;
-        this.speedY = 5;
-        this.lifeSpan = this.maxLength * 2;
-        this.breakPoint = this.lifeSpan * 0.85;
-        this.timer = 0;
-        this.angle = 0;
-        this.va = Math.random() * 0.5 - 0.25;
-        this.curve = 0.1;
-        this.vc = Math.random() * 0.4 - 0.2;
+class Particle {
+    constructor(effect) {
+        this.effect = effect
+        this.x = Math.floor(Math.random() * this.effect.width)
+        this.y = Math.floor(Math.random() * this.effect.height)
+        this.speedX = Math.random() * 5 - 2.5
+        this.speedY = Math.random() * 5 - 2.5
+        this.history = [{x: this.x, y: this.y}]
+        this.maxLength = Math.floor(Math.random() * 100 + 10)
+        this.angle = 0
     }
     draw(context){
-        //context.strokeStyle = 'hsl(' + this.hue + ', 100%, 50%)';
-        context.lineWidth = this.lineWidth;
-        context.beginPath();
-        context.moveTo(this.history[0].x, this.history[0].y);
-        for (let i = 0; i < this.history.length; i++){
-            context.lineTo(this.history[i].x, this.history[i].y);
+        context.fillRect(this.x, this.y, 5, 9)
+        context.beginPath()
+        context.moveTo(this.history[0].x, this.history[0].y)
+        for(let i = 1; i < this.history.length; i++){
+            context.lineTo(this.history[i].x, this.history[i].y)
         }
-        context.stroke();
+        context.stroke()
     }
     update(){
-        this.timer++;
-        this.angle += this.va;
-        this.curve += this.vc;
-        if (this.timer < this.lifeSpan){
-            if (this.timer > this.breakPoint){
-                this.va *= -1.12;
-            }
-            this.x += Math.sin(this.angle) * this.curve;
-            this.y += Math.cos(this.angle) * this.curve;
-            this.history.push({x: this.x, y: this.y});
-            if (this.history.length > this.maxLength){
-                this.history.shift();
-            }
-        } else if (this.history.length <= 1) {
-            this.reset();
-        } else {
-            this.history.shift();
+        this.angle += 0.5
+        this.x += this.speedX + Math.sin(this.angle) * 2
+        this.y += this.speedY - Math.cos(this.angle) * 3
+        this.history.push({x: this.x, y: this.y})
+        if(this.history.length > this.maxLength){
+            this.history.shift()
         }
     }
-    reset(){
-        this.x = Math.random() * this.canvas.width;
-        this.y = Math.random() * this.canvas.height;
-        this.history = [{x: this.x, y: this.y}];
-        this.timer = 0;
-        this.angle = 0;
-        this.curve = 0;
-        this.va = Math.random() * 0.5 - 0.25;
+}
+
+class Effect {
+    constructor(width, height) {
+        this.width = width
+        this.height = height
+        this.particles = []
+        this.numberOfParticles = 100
+        this.init()
+    }
+    init() {
+        //create particles
+        for(let i = 0; i < this.numberOfParticles; i++){
+            this.particles.push(new Particle(this))
+        }
+    }
+    render(context){
+        this.particles.forEach(particle => {
+            particle.draw(context)
+            particle.update()
+        })
     }
 }
 
-const linesArray = [];
-const numberOfLines = 40;
-for (let i = 0; i < numberOfLines; i++){
-    linesArray.push(new Line(canvas));
-}
-console.log(linesArray);
+const effect = new Effect(canvas.width, canvas.height)
+effect.render(ctx)
+console.log(effect)
 
-
-function animate(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // draw lines
-    linesArray.forEach(line => {
-        line.draw(ctx)
-        line.update()
-    });
-    requestAnimationFrame(animate);
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    effect.render(ctx)
+    requestAnimationFrame(animate)
 }
-animate();
+animate()
